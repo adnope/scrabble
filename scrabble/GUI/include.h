5d952ac58e6cf8c777c6846ea84e65378f9403db
dung195 <nguyentiendung100606@outlook.com.vn>
2025-07-14 07:36:25 +0000

    add GUI

---
+++ scrabble/GUI/include.h
@@ -0,0 +1,15 @@
+
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+// Standard library includes
+#include<bits/stdc++.h>
+
+// SDL2 includes
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
+
+// Project-specific includes
+// #include "some_project_header.h"
+
+#endif // INCLUDE_H
