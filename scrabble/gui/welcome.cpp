3ed919d8bdd09903e5753cc58a881b8ee3dd8897
dung195 <nguyentiendung100606@outlook.com.vn>
2025-07-15 07:49:40 +0000

    more change

--- scrabble/gui/welcome.cpp
+++ scrabble/gui/welcome.cpp
@@ -1,9 +1,9 @@
 #include "include.h"
 using namespace std;
-Image *background =new Image("assets/images/SCRABLE1.png", 0, 0, renderer);
+Image *background =new Image("assets/images/SCRABBLE1.png", 0, 0, renderer);
 Button_Image *startbutton= new Button_Image("assets/images/startbutton.png", 100, 100, renderer);
-Button_Image *quitbutton= new Button_Image("assets/images/startbutton.png", 200, 200, renderer);
-Button_Image* settings=new Button_Image("assets/images/settings.png", 300, 300, renderer);
+Button_Image *quitbutton= new Button_Image("assets/images/quitgame.png", 200, 200, renderer);
+Button_Image* settings=new Button_Image("assets/images/SETTING.png", 300, 300, renderer);
 void print_Welcome() {
   SDL_RenderClear(renderer);
   background->print_Image(renderer);
